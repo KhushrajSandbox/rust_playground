52515d7fdc125bb4ea3c99a8dbed72f57a7c10cc
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-03-23 20:04:01 +0530

    Add functions

---
+++ functions/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut number: i32 = get_five();
+    println!("Hello, world!");
+    println!("Number: {}", number);
+    add_4(&mut number);
+    println!("{}", number)
+}
+
+fn add_4 (mut x: &i32) -> () {
+    x = &4;
+}
+
+fn get_five() -> i32 {
+    5
+}
